34e46a58aea8b72ebeea2d4ddd738edf9b8597b9
amebotana <88494765+amebotana@users.noreply.github.com>
2021-08-11 00:26:21 -0300

    agregar comentarios a producto.js

--- js/products.js
+++ js/products.js
@@ -4,3 +4,9 @@
 document.addEventListener("DOMContentLoaded", function (e) {
 
 });
+
+//Realizar una petición web a una URL donde se encuentra una colección de productos en formato JSON, 
+//con la información básica (precio, nombre, breve descripción) respectiva a cada producto, y mostrar el 
+//listado en HTML.
+
+const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json";
