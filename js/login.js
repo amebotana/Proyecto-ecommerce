c3c4b2560b3a4ab42e50c4b98a38b679ec53a159
amebotana <88494765+amebotana@users.noreply.github.com>
2021-08-19 14:33:17 -0300

    se arreglo pagina login y arreglamos el form

--- js/login.js
+++ js/login.js
@@ -1,6 +1,21 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+document.getElementById("log"). addEventListener("click", myFunction);
+function myFunction(){
+var usuario = document.getElementById("inputEmail").value
+var contrasenia = document.getElementById("inputPassword").value
+if (usuario !="" && contrasenia !=""){
+    window.location.href = "home.html"
+} else {
+    alert("Debe completar los datos")
+}
+
+
+}
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+    myFunction();
 });
