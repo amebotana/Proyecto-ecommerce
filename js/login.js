b7a03a64785dfd63991f27c1b2c77656c03d3207
Amelia Botana <88494765+amebotana@users.noreply.github.com>
2021-08-22 20:37:05 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,9 +1,6 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-
-//document.getElementById("log").addEventListener("click", myFunction);
-
 function myFunction() {
     var usuario = document.getElementById("inputEmail").value;
     var contrasenia = document.getElementById("inputPassword").value;
