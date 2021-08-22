09a146a2b4fd4c59543d7b187eb562b85453762c
amebotana <88494765+amebotana@users.noreply.github.com>
2021-08-22 20:27:53 -0300

    actualizado el index a home en la barra de navegación //

--- js/login.js
+++ js/login.js
@@ -2,20 +2,21 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 
-document.getElementById("log"). addEventListener("click", myFunction);
-function myFunction(){
-var usuario = document.getElementById("inputEmail").value
-var contrasenia = document.getElementById("inputPassword").value
-if (usuario !="" && contrasenia !=""){
-    window.location.href = "home.html"
-} else {
-    alert("Debe completar los datos")
-}
+//document.getElementById("log").addEventListener("click", myFunction);
+
+function myFunction() {
+    var usuario = document.getElementById("inputEmail").value;
+    var contrasenia = document.getElementById("inputPassword").value;
+    if (usuario != "" && contrasenia != "") {
+        window.location.href = "home.html";
+    } else {
+        alert("Debe completar los datos");
+    }
 
 
 }
 
 
-document.addEventListener("DOMContentLoaded", function(e){
-    myFunction();
-});
+// document.addEventListener("DOMContentLoaded", function (e) {
+//     myFunction();
+// });
