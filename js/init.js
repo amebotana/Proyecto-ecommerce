0e706dcd41f024003ced37a4dc47a216bc046332
Amelia Botana <88494765+amebotana@users.noreply.github.com>
2021-11-24 02:19:54 -0300

    google log out

--- js/init.js
+++ js/init.js
@@ -66,6 +66,8 @@ document.addEventListener("DOMContentLoaded", function(e){
 function cerrarSesion(){
   sessionStorage.removeItem("Usuario");
   localStorage.clear()
+  let auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut()
   window.location.reload();
 }
 
@@ -74,17 +76,3 @@ function seguirComprando(){
 }
 
 
-function onSignIn(googleUser) {
-  // Useful data for your client-side scripts:
-  var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
-
-  // The ID token you need to pass to your backend:
-  var id_token = googleUser.getAuthResponse().id_token;
-  console.log("ID Token: " + id_token);
-}
